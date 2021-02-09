b98b4f1311a2f4a83b48a6001ccb964ad4a9d832
Ty-lavan-l <76555372+Ty-lavan-l@users.noreply.github.com>
2021-02-09 16:30:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import BasicExample from './components/EmployeeFinalLink';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <BasicExample/>
+      </>
+    </div>
+  );
+}
+
+export default App;
+
+// import './App.css';
+// import EBasicExample from './ecomponent/EmployeeFinalLink';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <>
+//       <EBasicExample/>
+//       </>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import PrimarySearchAppBar from './employueNavnar/EmployeeNavbar';
+
+// // import BasicExample from './router/RouterComp';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <>
+//       <PrimarySearchAppBar/>
+
+//      </>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+// import EmpForm from './formick/EmployeeForm';
+// import PrimarySearchAppBar from './components/Navbar';
+// import FormPropsTextFields from './components/Login&Register';
+// import WelcomePage from './components/WelcomePage';
+// import RegForm from './regt/RedForm';
+
+
+
+//  {/* <WelcomePage/> */}
+//      {/* <PrimarySearchAppBar/>
+//      <FormPropsTextFields/> */}
+//      {/* <RegForm/> */}
+
+
+
+
+
+// // import EmpForm from './formick/EmployeeForm';
+// // import PrimarySearchAppBar from './components/Navbar';
+// // import FormPropsTextFields from './components/Login&Register';
+// // import WelcomePage from './components/WelcomePage';
+// // import RegForm from './regt/RedForm';
+
+
+
+// //  {/* <WelcomePage/> */}
+// //      {/* <PrimarySearchAppBar/>
+// //      <FormPropsTextFields/> */}
+// //      {/* <RegForm/> */}
